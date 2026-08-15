861c5411725deb04963cf9a03b1ae75b99c60a34
Santhosh Reddy <theepireddysanthosh@gmail.com>
2026-08-15 11:06:38 -0400

    Configure stg_customers for incremental loading

--- stg_customers.sql
+++ stg_customers.sql
@@ -1,7 +1,22 @@
+{{ config(
+    materialized='incremental',
+    unique_key='chunk_id'
+) }}
+
 select
     franchiseID as franchise_id,
     review_date,
     chunked_text,
     chunk_id,
     review_uri
+
 from {{ source('default', 'san_test') }}
+
+{% if is_incremental() %}
+
+where review_date > (
+    select coalesce(max(review_date), '1900-01-01')
+    from {{ this }}
+)
+
+{% endif %}
